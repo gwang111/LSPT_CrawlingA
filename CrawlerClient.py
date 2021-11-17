48dd895259da6a992f3a28c222c9c91331187e53
Gary Wang <38331932+gwang111@users.noreply.github.com>
2021-11-16 23:08:28 -0500

    Crawler client that combines our crawler and priority queue. Sudo Code rn

---
+++ CrawlerClient.py
@@ -0,0 +1,31 @@
+#import SissiCrawler
+#import JessePriorityQueue
+import multiprocessing as mp
+
+class CrawlerClient():
+
+    def __init__(self): pass
+
+    def startCrawl(self, seed):
+        # parallel via processes
+        # initial crawl from seed using sissicrawler -> non-blocking
+        SissiCrawler crawler = SissiCrawler()
+
+        # start a new SissiCrawler for our initial crawl
+        crawler.crawl(seed)
+
+        JessePriorityQueue recrawl = None
+        while(True):
+            recrawl = self.getPriority()
+
+            # via multiprocessing Pool, we can create x amount of crawling processes for each recrawl URL
+
+            # collect finished crawls with blocking join maybe
+
+
+
+    def getPriority(self): 
+        # get jesse priority queue
+        # maybe through provided helper method, get URLs from priority queue that are due for a recrawl
+        # return as list of these URLS
+        return recrawl_list
