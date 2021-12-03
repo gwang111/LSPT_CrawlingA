4cc07e892dbc48e912d78f99a9f0083e584bb989
Alapocas <sissijian0720@163.com>
2021-12-03 06:15:02 -0500

    add example usages

--- SissiCrawler.py
+++ SissiCrawler.py
@@ -41,8 +41,15 @@ if __name__ == "__main__":
     rpiSeeds = ['https://rpi.edu', 'https://cipce.rpi.edu/index.html']
     wikiSeeds = ['https://en.wikipedia.org/wiki/Tiger', 'https://zh.wikipedia.org/wiki/%E8%99%8E']
     rpiSeed = ['https://rpi.edu']
-    wikiSeed = ['https://en.wikipedia.org/wiki/Tiger']    
-    a = SissiCrawler("rpi", rpiSeed, None, 30, None, None)
+    wikiSeed = ['https://en.wikipedia.org/wiki/Tiger']
+    # you can pass in {} or set() to the crawler creator if you want to set crawledUrls / badUrls to be global var
+    a = SissiCrawler("rpi", rpiSeed, None, 30, None, None) # rpi crawler
+    #a = SissiCrawler("rpi", rpiSeeds, None, 30, None, None) # rpi crawler, threaded
+    #a = SissiCrawler("wiki", wikiSeed, None, 30, None, None) # wiki crawler
+    #a = SissiCrawler("wiki", wikiSeeds, None, 30, None, None) # wiki crawler threaded
+    #a = SissiCrawler("github", None, None, 30, None, None) # github crawler
+    #a = SissiCrawler("reddit", None, None, 30, None, None) # reddit crawler
+    #a = SissiCrawler("reddit", None, 2, 30, None, None) # reddit crawler, threaded
     a.crawl()
     print("crawledUrsl:", len(a.crawledUrls))
     print("badUrls:", len(a.badUrls))
