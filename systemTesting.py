ae8d54e07c2cb2a2388771182fe7fc3f3b15578a
Gary Wang <38331932+gwang111@users.noreply.github.com>
2021-12-08 10:34:31 -0500

    Update systemTesting.py
    Adding in test structure

--- systemTesting.py
+++ systemTesting.py
@@ -3,9 +3,68 @@ import SissiCrawler
 
 import unittest
 
-class CrawlerTests(unittest.TestCase):
-    def test(): pass
+class SystemTestSuite(unittest.TestCase):
 
-class PriorityQueueTests(unittest.TestCase): 
+    def sys_1(): pass
 
-    def test(): pass
+    def sys_2(): pass
+
+    def sys_3(): pass
+
+    def sys_4(): pass
+
+    def sys_5(): pass
+
+    def sys_6(): pass
+
+    def sys_7(): pass
+
+    def sys_8(): pass
+
+    def sys_9(): pass
+
+    def sys_10(): pass
+
+    def sys_11(): pass
+
+    def sys_12(): pass
+
+    def sys_13(): pass
+
+    def sys_14(): pass
+
+    def sys_15(): pass
+
+    def sys_16(): pass
+
+    def sys_17(): pass
+
+    def sys_18(): pass
+
+    def sys_19(): pass
+
+    def sys_20(): pass
+
+    def sys_21(): pass
+
+    def sys_22(): pass
+
+    def sys_23(): pass
+
+    def sys_24(): pass
+
+    def sys_25(): pass
+
+    def sys_26(): pass
+
+    def sys_27(): pass
+
+    def sys_28(): pass
+
+    def sys_29(): pass
+
+    def sys_30(): pass
+
+    def sys_31(): pass
+
+    def sys_32(): pass
